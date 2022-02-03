61fc58d9287c164b78c2249d51b6c7286b8843e0
Andry Kalaсh <craftmail99@mail.ru>
2022-02-03 22:52:41 +0500

    закончен урок 02

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const a = 1
-const b = "Наша ветка lesson01"
+let title, screens, screenPrice, rollback, fullPrice, adaptive;
+title = "lesson02";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 123;
+rollback = 85;
+fullPrice = 100000;
+adaptive = true;
 
-const logged = () => {
-  console.log('Наконец мы узнали все команды, необходимые для работы с GIT');
-}
+//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+
+//Вывести в консоль длину строки screens
+console.log(screens.length);
+
+//Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+
+let sPrice = `"Стоимость верстки экранов ${screenPrice} рублей / долларов / гривен / юани”`
+console.log(sPrice);
+
+let fPrice = `"Стоимость разработки сайта ${fullPrice} рублей / долларов / гривен / юани"`
+console.log(fPrice);
+
+//Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+screens = screens.toLowerCase();
+
+const myArraySplits = screens.trim().split(", ");
+console.log(myArraySplits);
+
+//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+
+let myFullPrice = fullPrice * (rollback / 100);
+console.log(myFullPrice);
