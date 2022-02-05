533548d9b9d1e431d10a4751af0335c7bf481da9
Andry Kalaсh <craftmail99@mail.ru>
2022-02-05 23:50:00 +0500

    заново создал lesson03 выполнил задания

--- script.js
+++ script.js
@@ -34,3 +34,39 @@ console.log(myArraySplits);
 let myFullPrice = fullPrice * (rollback / 100);
 console.log(myFullPrice);
 
+//Задания урок 3
+// 3
+title = prompt("Как называется ваш проект?");
+console.log(title);
+// 4
+screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
+// 5
+screenPrice = +prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
+// 6
+adaptive = confirm("Нужен ли адаптив на сайте?");
+console.log(adaptive);
+// 7
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+//8
+fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+console.log(fullPrice);
+// 9
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+//10
+switch (true) {
+  case fullPrice >= 3e4:
+    console.log("Даем скидку в 10%");
+    break;
+  case (fullPrice < 3e4) && (fullPrice >= 15e3):
+    console.log("Даем скидку в 5%");
+    break;
+  case (fullPrice < 15e3) && (fullPrice >= 0):
+    console.log("Скидка не предусмотрена");
+    break;
+}
