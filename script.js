8a702eaa4b6f13f4597a6eee9c417e2afc788b86
Andry Kalaсh <craftmail99@mail.ru>
2022-02-08 00:40:14 +0500

    сделал половину задания урока4

--- script.js
+++ script.js
@@ -1,83 +1,98 @@
 "use strict";
 //блок объявления переменных
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-if (screenPrice === '' || isNaN(screenPrice)) {
-    alert('Было введено не число, попробуйте еще раз ввести стоимость разработки экранов');
+let title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+let screens = prompt("Какие типы экранов нужно разработать?", "Обычные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "10000");
+if (screenPrice === "" || isNaN(screenPrice)) {
+  alert(
+    "Было введено не число, попробуйте еще раз ввести стоимость разработки экранов"
+  );
 }
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-if (servicePrice1 === '' || isNaN(servicePrice1)) {
-    alert('Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №1');
+let service1 = prompt(
+  "Какой дополнительный тип услуги нужен?",
+  "Поиск на сайте"
+);
+let servicePrice1 = +prompt("Сколько это будет стоить?", 15000);
+if (servicePrice1 === "" || isNaN(servicePrice1)) {
+  alert(
+    "Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №1"
+  );
 }
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-if (servicePrice2 === '' || isNaN(servicePrice2)) {
-    alert('Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №2');
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "Экспресс");
+let servicePrice2 = +prompt("Сколько это будет стоить?", 5000);
+if (servicePrice2 === "" || isNaN(servicePrice2)) {
+  alert(
+    "Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №2"
+  );
 }
 let rollback = 10;
-let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+let allServicePrices;
 
 // блок объявления функций
+//1
+const getAllServicePrices = function (ser1, ser2) {
+  return ser1 + ser2;
+};
+//2
+function getFullPrice(scr, asp) {
+  return scr + asp;
+}
+//3
+const getTitle = function (title) {
+  return;
+};
+//4
+const getServicePercentPrices = function (fuPr) {
+  return Math.ceil(fuPr - fuPr * (rollback / 100));
+};
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
-}
+};
 
 const getRollbackMessage = function (price) {
   if (price >= 30000) {
-      return "Даем скидку в 10%"
-  }  else if (price >= 15000 && price < 30000) {
-     return "Даем скидку в 5%"
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку в 5%";
   } else if (price < 15e3 && price >= 0) {
-      return "Скидка не предусмотрена"
+    return "Скидка не предусмотрена";
   } else {
-    return "Что то пошло не так"
+    return "Что то пошло не так";
   }
-}
-/*
-// Проверка введенного числа, числовое или нет.
-if (screenPrice === '' || isNaN(screenPrice)) {
-    console.log('Было введено не число, попробуйте еще раз ввести стоимость разработки экранов');
-}
-
-if (servicePrice1 === '' || isNaN(servicePrice1)) {
-    console.log('Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №1');
-}
-
-if (servicePrice2 === '' || isNaN(servicePrice2)) {
-    console.log('Было введено не число, попробуйте еще раз ввести стоимость дополнительной услуги №2');
-}
-
-*/
+};
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice);
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof screenPrice);
 console.log(typeof adaptive);
+
+console.log("Общая сумма разработчику " + getServicePercentPrices(fullPrice));
+console.log("Сколько скидка?: " + getRollbackMessage(fullPrice));
+console.log("Дополнительные услуги стоят: " + allServicePrices);
+
 /*
 const myArraySplits = screens.trim().split(", ");
 console.log(myArraySplits);
 */
-
-
-
 /*
 switch (true) {
   case fullPrice >= 3e4:
     console.log("Даем скидку в 10%");
     break;
-  case (fullPrice < 3e4) && (fullPrice >= 15e3):
+  case fullPrice < 3e4 && fullPrice >= 15e3:
     console.log("Даем скидку в 5%");
     break;
-  case (fullPrice < 15e3) && (fullPrice >= 0):
+  case fullPrice < 15e3 && fullPrice >= 0:
     console.log("Скидка не предусмотрена");
     break;
 }
